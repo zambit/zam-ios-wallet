7551feaf4daae842369083a9db5088ad444e4b42
Oleg Antonov <zamleg@zam.me>
2018-07-06 17:10:04 +0300

    tutorial screens prototype

--- Screens/AuthViewController.swift
+++ Screens/AuthViewController.swift
@@ -9,13 +9,31 @@
 import UIKit
 
 class AuthViewController: UIViewController {
+    @IBOutlet var welcome_screen_0: UIView!
+    @IBOutlet var welcome_screen_1: UIView!
+    
     @IBOutlet weak var registrationButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.registrationButton.layer.backgroundColor = UIColor(white: 0.8, alpha: 1).cgColor
-        self.registrationButton.layer.cornerRadius = 5
-        self.registrationButton.layer.masksToBounds = false
+        //
+        registrationButton.layer.backgroundColor = UIColor(white: 0.8, alpha: 1).cgColor
+        registrationButton.layer.cornerRadius = 5
+        registrationButton.layer.masksToBounds = false
+        //
+        scrollView.alwaysBounceHorizontal = true
+        scrollView.addSubview(welcome_screen_0)
+        scrollView.addSubview(welcome_screen_1)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        let width = self.view.frame.width
+        let height = scrollView.frame.height
+        welcome_screen_0.frame = CGRect(x: 0, y: 0, width: width, height: height)
+        welcome_screen_1.frame = CGRect(x: width, y: 0, width: width, height: height)
+        scrollView.contentSize = CGSize(width: width * 2, height: height)
     }
     
     @IBAction func onRegistrationButton(_ sender: Any) {
