3f0029c2ca8f8a208cd0ada8adec2b8617305b6c
Alex Ponomarev <alexpon@zam.me>
2018-07-23 17:49:25 +0300

    add ControllerHelper

---
+++ Helpers/ControllerHelper.swift
@@ -0,0 +1,22 @@
+//
+//  ControllerHelper.swift
+//  what-2-watch
+//
+
+import Foundation
+import UIKit
+
+struct ControllerHelper {
+    static func getTopViewController() -> UIViewController? {
+        var topVC = UIApplication.shared.keyWindow?.rootViewController
+        while((topVC!.presentedViewController) != nil) {
+            topVC = topVC!.presentedViewController
+        }
+        return topVC
+    }
+
+    static func instantiateViewController(identifier id: String, storyboardName: String = "Main") -> UIViewController {
+        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+        return storyboard.instantiateViewController(withIdentifier: id)
+    }
+}
