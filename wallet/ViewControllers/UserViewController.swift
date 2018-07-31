65faa7cafcdd439f5cc4cf2fe0deac95fb71c10b
Alex Ponomarev <git@gitlab.com>
2018-07-31 20:07:34 +0300

    add UserViewController

--- wallet/ViewControllers/UserViewController.swift
+++ wallet/ViewControllers/UserViewController.swift
@@ -9,9 +9,31 @@
 import Foundation
 import UIKit
 
-class UserViewController: UIViewController {
+class UserViewController: FlowViewController {
+
+    var userManager: WalletUserDefaultsManager?
+
+    @IBOutlet var tokenLabel: UILabel?
+    @IBOutlet var phoneNumberLabel: UILabel?
+
+    private var authToken: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let phone = userManager?.getPhoneNumber() {
+            phoneNumberLabel?.text = phone
+        } else {
+            phoneNumberLabel?.text = "Phone cant extract from UserDefaults"
+        }
+
+        tokenLabel?.text = authToken
+
+        setupDefaultStyle()
+    }
 
     func prepare(authToken: String) {
+        self.authToken = authToken
         print("Auth token: \(authToken)")
     }
 
