db000ef09473b775c0a84c2162a61606ca304098
Alex Ponomarev <git@gitlab.com>
2018-10-22 16:15:22 +0300

    prepare for merge

--- wallet/View/Cells/TextFieldTableViewCell/TextFieldTableViewCellData.swift
+++ wallet/View/Cells/TextFieldTableViewCell/TextFieldTableViewCellData.swift
@@ -1,5 +1,5 @@
 //
-//  TextFieldCellData.swift
+//  TextFieldTableViewCellData.swift
 //  wallet
 //
 //  Created by Alexander Ponomarev on 10/09/2018.
@@ -8,18 +8,18 @@
 
 import UIKit
 
-enum TextFieldCellDataAction {
+enum TextFieldTableViewCellDataAction {
     case tap((UITextField) -> Void)
     case editingChanged((UITextField) -> Void)
 }
 
-struct TextFieldCellData {
+struct TextFieldTableViewCellData {
 
     let placeholder: String
     let keyboardType: UIKeyboardType?
     let autocapitalizationType: UITextAutocapitalizationType?
     let autocorrectionType: UITextAutocorrectionType?
 
-    let action: TextFieldCellDataAction?
-    let beginEditingAction: ((TextFieldCell) -> Void)?
+    let action: TextFieldTableViewCellDataAction?
+    let beginEditingAction: ((TextFieldTableViewCell) -> Void)?
 }
